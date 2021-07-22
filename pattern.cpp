df97f08f99a0da49342fe1aea9c1da82ecd9cc3c
Maximka123124 <52974908+Maximka123124@users.noreply.github.com>
2021-07-22 17:21:35 +0400

    Create pattern.cpp
    Небольшой шаблон программ на с++

---
+++ pattern.cpp
@@ -0,0 +1,16 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <string>
+#define endl '\n'
+using namespace std;
+
+int main() {
+#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin); freopen("output.txt", "w", stdout);
+#endif // !ONLINE_JUDGE
+	ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+
+	return 0;
+}
