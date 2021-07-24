d7df4ca22182b623563774b41d1735530dc3d86b
MXCode <52974908+Maximka123124@users.noreply.github.com>
2021-07-24 12:53:27 +0400

    Update dfs.cpp

--- dfs.cpp
+++ dfs.cpp
@@ -1,61 +1,60 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <iostream>
 #include <vector>
+#include <algorithm>
 #include <string>
+#define endl '\n'
 using namespace std;
 
-int n;
-vector <vector <int>> g;
-int used[100];
-bool tree = true;
-
-void dfs(int i, int from = -1) {
-	if ((used[i] == 1 && from != -1 && g[i][from] != 1) || !tree){
-		tree = false;
-		return;
-	}
-	else if (used[i] == 1 && from != -1 && g[i][from] == 1) {
-		return;
-	}
-	else {
-		used[i] = 1;
-	}
-	for (int j = 0; j < n; ++j) {
-		if (g[i][j])
-			dfs(j, i);
-	}
-}
-
 int main() {
-	cin >> n;
-	string s;
-	for (int i = 0; i < n; ++i) {
-		g.push_back(vector<int>());
-		int link;
+#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin); freopen("output.txt", "w", stdout);
+#endif // !ONLINE_JUDGE
+	ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+
+	int num;
+	cin >> num;
+	int n, k;
+	for (int i = 0; i < num; ++i) {
+		cin >> n >> k;
+		vector <int> m(n);
+		vector <int> cnt(200000, 0);
 		for (int j = 0; j < n; ++j) {
-			cin >> link;
-			g[i].push_back(link);
+			cin >> m[j];
+			cnt[m[j] - 1]++;
 		}
+		int c = 0;
+		for (int j = 0; j < 200000; ++j) {
+			if (cnt[j] > k) {
+				cnt[j] = -cnt[j];
+			}
+			if (cnt[j] <= k && cnt[j] > 0)
+				c += cnt[j];
+		}
+
+		int color = 1;
+		for (int j = 0; j < n; ++j) {
+			if (abs(cnt[m[j]-1]) > k) {   //FIXME
+				cout << 0 << " ";
+				cnt[m[j] - 1]--;
+			}
+			else if (cnt[m[j]-1] < 0) {
+				if (c >= k) {
+					cout << color % k << " ";
+					cnt[m[j]-1] += 1;
+					color++;
+				}
+				else
+					cout << 0 << " ";
+				
+			}
+			else {
+				cout << cnt[m[j] - 1] << " ";
+				cnt[m[j] - 1]--;
+			}
+		}
+
 	}
 
-	dfs(0);
-	bool ok = true;
-	for (int i = 0; i < n; ++i) {
-		if (used[i] == 0) {
-			ok = false;
-			break;
-		}
-	}
-
-	if (tree && ok) cout << "YES";
-	else cout << "NO";
 	return 0;
 }
-
-
-// FIXME:
-// 3
-// 0 1 1
-// 1 0 1
-// 1 1 0
-// Это не дерево, т.к. здесть есть цикл: из 1 в 2, из 2 в 3, из 3 в 1
