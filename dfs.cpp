6b258fb6b70c17a625917e219cdd97aadabc6684
MXCode <52974908+Maximka123124@users.noreply.github.com>
2021-07-25 12:45:20 +0400

    Update dfs.cpp

--- dfs.cpp
+++ dfs.cpp
@@ -2,9 +2,40 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
+#include <cctype>
 #include <string>
 #define endl '\n'
 using namespace std;
+
+vector <vector <int>> go = { { -1, 0 },{ 0, -1 },{ 0, 1 },{ 1, 0 } };
+vector <vector <int>> used;
+int used_now = 0, cnt = 0;
+
+void dfs(int i, int j) {
+	int moves = 0;
+	used[i][j] = 1;
+	for (int k = 0; k < 4; ++k) {
+		int i2 = i + go[k][0], j2 = j + go[k][1];
+		if (i2 == 0 || i2 == used.size() - 1 || j2 == 0 || j2 == used[0].size() - 1)
+			continue;
+		if (used[i2][j2] == 0) {
+			used_now++;
+			moves += 1;
+			dfs(i2, j2);
+		}
+	}
+
+	if (i==1 && j==1) {
+		for (int k = 1; k < used.size(); ++k) {
+			for (int f = 1; f < used[0].size(); ++f) {
+				if (used[k][f] == 0) {
+					dfs(k, f);
+					cnt++;
+				}
+			}
+		}
+	}
+}
 
 int main() {
 #ifndef ONLINE_JUDGE
@@ -12,49 +43,38 @@ int main() {
 #endif // !ONLINE_JUDGE
 	ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
 
-	int num;
-	cin >> num;
-	int n, k;
-	for (int i = 0; i < num; ++i) {
-		cin >> n >> k;
-		vector <int> m(n);
-		vector <int> cnt(200000, 0);
-		for (int j = 0; j < n; ++j) {
-			cin >> m[j];
-			cnt[m[j] - 1]++;
+	int n, m;
+	cin >> n >> m;
+	string s;
+	used.push_back(vector<int>(m + 2, -1));
+	for (int i = 1; i <= n; ++i) {
+		cin >> s;
+		used.push_back(vector<int>(1, -1));
+		for (int j = 0; j < m; ++j) {
+			if (s[j] == '#')
+				used[i].push_back(0);
+			else
+				used[i].push_back(-1);
 		}
-		int c = 0;
-		for (int j = 0; j < 200000; ++j) {
-			if (cnt[j] > k) {
-				cnt[j] = -cnt[j];
-			}
-			if (cnt[j] <= k && cnt[j] > 0)
-				c += cnt[j];
-		}
-
-		int color = 1;
-		for (int j = 0; j < n; ++j) {
-			if (abs(cnt[m[j]-1]) > k) {   //FIXME
-				cout << 0 << " ";
-				cnt[m[j] - 1]--;
-			}
-			else if (cnt[m[j]-1] < 0) {
-				if (c >= k) {
-					cout << color % k << " ";
-					cnt[m[j]-1] += 1;
-					color++;
-				}
-				else
-					cout << 0 << " ";
-				
-			}
-			else {
-				cout << cnt[m[j] - 1] << " ";
-				cnt[m[j] - 1]--;
-			}
-		}
-
+		used[i].push_back(-1);
 	}
+	used.push_back(vector<int>(m + 2, -1));
+	if (used[1][1] == 0) {
+		cnt++;
+		dfs(1, 1);
+	}
+	else {
+		for (int k = 1; k < used.size(); ++k) {
+			for (int f = 1; f < used[0].size(); ++f) {
+				if (used[k][f] == 0) {
+					cnt++;
+					dfs(k, f);
+					break;
+				}
+			}
+		}
+	}
+	cout << cnt;
 
 	return 0;
 }
