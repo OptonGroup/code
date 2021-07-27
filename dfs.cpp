e8ab149ebb4ad520d2bfbd20e935adee47115959
MXCode <52974908+Maximka123124@users.noreply.github.com>
2021-07-27 17:48:50 +0400

    Update dfs.cpp

--- dfs.cpp
+++ dfs.cpp
@@ -10,21 +10,23 @@ using namespace std;
 
 int n;
 vector <int> nums;
+vector <int> out;
 
-int dp(int num){
-    if (nums[num-1] != -1){
-        return nums[num-1];
-    }
-    
-    if (num%2 != 0){
-        nums[num-1] = dp(num-1)+1;
-    } else{
-        nums[num-1] = dp(num/2)+1;
-    }
-    if (num%3 == 0){
-        nums[num-1] = min(dp(num/3)+1, nums[num-1]);
-    }
-    return nums[num-1];
+int dp(int num) {
+	if (nums[num - 1] != -1) {
+		return nums[num - 1];
+	}
+
+	if (num % 2 != 0) {
+		nums[num - 1] = dp(num - 1) + 1;
+	}
+	else {
+		nums[num - 1] = dp(num / 2) + 1;
+	}
+	if (num % 3 == 0) {
+		nums[num - 1] = min(dp(num / 3) + 1, nums[num - 1]);
+	}
+	return nums[num - 1];
 }
 
 int main() {
@@ -32,24 +34,30 @@ int main() {
 	freopen("input.txt", "r", stdin); freopen("output.txt", "w", stdout);
 #endif // !ONLINE_JUDGE
 	ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
-	
-    cin >> n;
-    nums.resize(n*2, -1);
-    nums[0] = 0;
-    int ans = dp(n);
-    while (n != 1){
-        if ((nums[n-1] == nums[n/3-1]+1) && (n%3 == 0)){
-            cout << 3;
-            n /= 3;
-        } else if((nums[n-1] == nums[n/2-1]+1) && (n%2 == 0)){
-            cout<<2;
-            n/=2;
-        } else{
-            cout << 1;
-            n-=1;
-        }
-    }
-    
+
+	cin >> n;
+	nums.resize(n * 2, -1);
+	nums[0] = 0;
+	int ans = dp(n);
+	vector <int> out;
+	while (n != 1) {
+		if (n > 2 && (nums[n / 2 - 1] > nums[n / 3 - 1]) && (nums[n - 1] == nums[n / 3 - 1] + 1) && (n % 3 == 0)) {
+			out.push_back(3);
+			n /= 3;
+		}
+		else if (n > 1 && (nums[n - 1] == nums[n / 2 - 1] + 1) && (n % 2 == 0)) {
+			out.push_back(2);
+			n /= 2;
+		}
+		else {
+			out.push_back(1);
+			n -= 1;
+		}
+	}
+	for (int i = out.size()-1; i >= 0; --i) {
+		cout << out[i];
+	}
+
 
 	return 0;
 }
