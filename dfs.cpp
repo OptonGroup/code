a2e2b365ff673ae0dec96f5ddad353916bb0902f
MXCode <52974908+Maximka123124@users.noreply.github.com>
2021-08-03 12:50:00 +0400

    Update dfs.cpp

--- dfs.cpp
+++ dfs.cpp
@@ -8,102 +8,61 @@
 #define endl '\n'
 using namespace std;
 
-int n;
-vector <int> nums;
-vector <int> out;
-
-int dp(int num) {
-	if (nums[num - 1] != -1) {
-		return nums[num - 1];
+struct bags_struct{
+    int time, type_, day;
+	bags_struct() {}
+	bags_struct(int time_, int type__, int day_) {
+		time = time_;
+		type_ = type__;
+		day = day_;
 	}
-
-	if (num % 2 != 0) {
-		nums[num - 1] = dp(num - 1) + 1;
-	}
-	else {
-		nums[num - 1] = dp(num / 2) + 1;
-	}
-	if (num % 3 == 0) {
-		nums[num - 1] = min(dp(num / 3) + 1, nums[num - 1]);
-	}
-	return nums[num - 1];
+	bool operator<(const bags_struct & b) const{
+		if (day != b.day)
+			return day < b.day;
+		if (time != b.time)
+			return time < b.time;
+		if (type_ != b.type_)
+			return type_ < b.type_;
+    }
 }
 
-int main() {
-#ifndef ONLINE_JUDGE
-	freopen("input.txt", "r", stdin); freopen("output.txt", "w", stdout);
-#endif // !ONLINE_JUDGE
-	ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
-
+; int main() {
+	int n;
 	cin >> n;
-	nums.resize(n * 2, -1);
-	nums[0] = 0;
-	int ans = dp(n);
-	vector <int> out;
-	while (n != 1) {
-		if (((nums[n/3-1] < nums[n/2-1])||(nums[n / 3 - 1] < nums[n-1])) && n%3 == 0 && nums[n/3-1] != -1) {
-			out.push_back(3);
-			n /= 3;
+	vector<bags_struct> bags;
+	for (int i = 0; i < n; ++i) {
+		int h, m, s;
+		cin >> h >> m >> s;
+		int t_s = h * 3600 + m * 60 + s;
+		cin >> h >> m >> s;
+		int t_e = h * 3600 + m * 60 + s;
+		int day = 0;
+		if (t_s >= t_e) {
+			bags.push_back(bags_struct({ t_s, 0, 0 }));
+			bags.push_back(bags_struct({ 86400, 1, 0 }));
+			day++;
 		}
-		else if ((nums[n / 2 - 1] < nums[n - 1])&&n%2==0 && nums[n/2-1] != -1) {
-			out.push_back(2);
-			n /= 2;
-		}
-		else {
-			out.push_back(1);
-			n -= 1;
-		}
-	}
-	for (int i = out.size()-1; i >= 0; --i) {
-		cout << out[i];
+
+		bags.push_back(bags_struct({ 0, 0, day }));
+		bags.push_back(bags_struct({ t_e, 1, day }));
 	}
 
-
+	int cnt = 0;
+	int time = 0;
+	int last_time = 0;
+	sort(bags.begin(), bags.end());
+ 	for (auto & i : bags) {
+		if (i.type_ == 0) {
+			cnt++;
+		}
+		else if (i.type_ == 1) {
+			cnt--;
+			if (cnt == n-1) {
+				time += i.time - last_time;
+			}
+		}
+		last_time = i.time;
+	}
+	cout << time;
 	return 0;
-}
-
-
-struct Event(){
-    int x, type_, index;
-    Event(){}
-    Event(int x, int type_, int index){
-        x(x) = x; type_(type) = type_; index(index) = index;
-    }
-    bool operator<(const Event, & b) const{
-        if (x != b.x)
-            return x < b.x;
-        if (type_ != b.type_)
-            return type_ < b.type_;
-        return index < b.index;
-    }
-}
-
-int n, m;
-cin >> n >> m;
-vector<Event> events;
-for (int i = 0; i < n; ++i){
-    int a, b;
-    cin >> a >> b;
-    if (a > b)
-        swap(a, b);
-    events.push_back(Event({a, 0, i}))
-    events.push_back(Event({b, 2, i}))
-}
-
-vector <int> ans(m,0);
-for(int i = 0; i < m; ++i){
-    cin >> a;
-    events.push_back(Event({a, 0, i}));
-}
-
-int cnt = 0;
-sort(events.begin(), events.end())
-for (auto & i: events){
-    if (i.type_ == 0){
-        cont++;   
-    }
-    else if (i.type_ == 1){
-        cont--;   
-    }
-    
 }
