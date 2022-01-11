cfcb321713516fec55321600f44a87ab1333e291
MXCode <52974908+MXCode-official@users.noreply.github.com>
2022-01-11 19:21:04 +0400

    Update pattern.cpp

--- pattern.cpp
+++ pattern.cpp
@@ -6,26 +6,17 @@
 // || //\\ ||   CC      O    O   D    D   E
 // ||//  \\||    CCCCC   OOOO    DDDDD    EEEEEE
 //
-
-
-#define _CRT_SECURE_NO_WARNINGS
 #include <bits/stdc++.h>
-#include <functional>
-#include <algorithm>
-#include <iostream>
-#include <iomanip>  // fixed << setprecision(n)
-#include <vector>
-#include <cctype>
-#include <string>
-#define endl '\n'
+#define endl '\n'                                       
 typedef long long ll;
 using namespace std;
 
 int main() {
 #ifndef ONLINE_JUDGE
-	freopen("input.txt", "r", stdin); freopen("output.txt", "w", stdout);
+ 	freopen("input.txt", "r", stdin); freopen("output.txt", "w", stdout);
 #endif // !ONLINE_JUDGE
-	ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+ 	ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);	
+
 
 
 	cerr << "Time: " << (float)clock() / CLOCKS_PER_SEC << " secs" << endl;
