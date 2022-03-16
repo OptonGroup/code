3f2801227512e143f271964ecfc09c90bb6f819c
MXCode <52974908+MXCode-official@users.noreply.github.com>
2022-03-16 17:00:17 +0400

    Create pattern_vs.cpp

---
+++ pattern_vs.cpp
@@ -0,0 +1,25 @@
+// Botat' every day
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <functional>
+#include <cctype>
+#include <string>
+#include <iomanip>
+#define endl '\n'
+#define all(x) x.begin(), x.end()
+typedef long long ll;
+using namespace std;
+
+int main() {
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin); freopen("output.txt", "w", stdout);
+#endif // !ONLINE_JUDGE 
+    ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+
+
+
+    cerr << "Time: " << (float)clock() / CLOCKS_PER_SEC << " secs" << endl;
+    return 0;
+}
