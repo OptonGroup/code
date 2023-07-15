e7920d1e54198959f69d1ad30be5834d9ff124e3
Opton <52974908+OptonGroup@users.noreply.github.com>
2023-07-15 08:56:16 +0300

    Update pattern_vs.cpp

--- pattern_vs.cpp
+++ pattern_vs.cpp
@@ -18,21 +18,22 @@
 #include <set>
 #include <queue>
 #include <cmath>
-#define endl '\n'
+#define endl "\n"
 #define all(x) x.begin(), x.end()
 #define print(x) for (auto u: x) cout << u << " ";
+#define getv(v) for (int i = 0; i < v.size(); i++) cin >> v[i];
 typedef long long ll;
 typedef long double ld;
 using namespace std;
 
 int main() {
 #ifndef ONLINE_JUDGE
-    freopen("input.txt", "r", stdin); freopen("output.txt", "w", stdout);
-#endif // !ONLINE_JUDGE 
-    ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+ 	freopen("input.txt", "r", stdin); freopen("output.txt", "w", stdout);
+#endif // !ONLINE_JUDGE
+ 	ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);	
 
 
 
-    cerr << "Time: " << (float)clock() / CLOCKS_PER_SEC << " secs" << endl;
-    return 0;
+	cerr << "Time: " << (float)clock() / CLOCKS_PER_SEC << " secs" << endl;
+	return 0;
 }
